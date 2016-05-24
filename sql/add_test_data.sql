b02c5c7c6b4d4cf51dc3e1dae4baac50011045b5
termanty <termanty@gmail.com>
2016-05-24 14:57:55 +0300

    Database initialization

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,16 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+
+INSERT INTO Member (username, password) VALUES ('tero', 'tero1');
+INSERT INTO Member (username, password) VALUES ('kaisa', 'kaisa1');
+INSERT INTO Member (username, password) VALUES ('kai', 'kai1');
+INSERT INTO Member (username, password) VALUES ('suvi', 'suvi1');
+
+INSERT INTO Topic (member_id, title, published) VALUES 
+(1, 'New SLS launcher', NOW());
+
+
+
+INSERT INTO Message (topic_id, msg, published) VALUES 
+(1, 'Because it seems that this project start to get realized, I want to give some more detais of this project', NOW());
+
+INSERT INTO Tag (name) VALUES ('NASA');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,29 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+
+CREATE TABLE Member(
+    id SERIAL PRIMARY KEY,
+    username varchar(15) NOT NULL,
+    password varchar(30) NOT NULL,
+    member_from DATE
+);
+
+
+CREATE TABLE Topic(
+    id SERIAL PRIMARY KEY,
+    member_id INTEGER REFERENCES Member(id),
+    title varchar(100) NOT NULL,
+    published DATE
+);
+
+CREATE TABLE Message(
+    id SERIAL PRIMARY KEY,
+    topic_id INTEGER REFERENCES Topic(id),
+    msg TEXT NOT NULL,
+    published DATE
+);
+
+CREATE TABLE Tag(
+    id SERIAL PRIMARY KEY,
+    name varchar(20) NOT NULL,
+    description varchar(150)
+);
